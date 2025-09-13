26f16286bc3aee83e2bb40e2cc1a4232d961c336
Ghizlane <belkhanfarghizlane@gmail.com>
2025-09-13 21:28:50 +0000

    fix task 7 : print c is fun x times with loop and only two console

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,18 @@
-const times = parseInt(process.argv[2], 10);
+// prints x times "C is fun"
+// usage: node 7-multi_c.js <times>
 
-if (Number.isNaN(times)) {
+const n = parseInt(process.argv[2], 10);
+
+if (Number.isNaN(n)) {
+  // 1st (and only) console.log in the "error" path
   console.log("Missing number of occurrences");
 } else {
   let out = "";
-  for (let i = 0; i < times; i++) {
-    out += "C is fun" + (i < times - 1 ? "\n" : "");
+  // you MUST use a loop
+  for (let i = 0; i < n; i++) {
+    out += "C is fun";
+    if (i < n - 1) out += "\n";
   }
-  console.log(out); // second (and last) log
+  // 2nd (and only) console.log in the "success" path
+  if (n > 0) console.log(out); // prints nothing for n <= 0 (e.g., -3)
 }
